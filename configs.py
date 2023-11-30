5ae4cc26ef8fb9b87cc8dc7e72cf3bd1c49bec96
Aviral Agrawal <avi.bpgc@gmail.com>
2023-11-30 18:10:02 -0500

    Update configs.py with correct links
    Added the huggingface links

--- configs.py
+++ configs.py
@@ -1,6 +1,6 @@
 modeltype2path = {
-    "Llama-2-7b-chat-hf": "",
-    "Llama-2-13b-chat-hf": "",
-    "Llama-2-7b-hf": "",
-    "Llama-2-13b-hf": "",
+    "Llama-2-7b-chat-hf": "meta-llama/Llama-2-7b-chat-hf",
+    "Llama-2-13b-chat-hf": "meta-llama/Llama-2-13b-chat-hf",
+    "Llama-2-7b-hf": "meta-llama/Llama-2-7b-hf",
+    "Llama-2-13b-hf": "meta-llama/Llama-2-13b-hf",
 }
